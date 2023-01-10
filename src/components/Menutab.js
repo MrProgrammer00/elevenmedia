6edacc3f6eff471321ed477e8a5cbc964cb799c1
MrProgrammer00 <88051679+MrProgrammer00@users.noreply.github.com>
2023-01-10 14:55:50 +0530

    first commit

---
+++ src/components/Menutab.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../App.css"
+
+function Menutab() {
+    return(
+        <div className="menutab">
+            
+        </div>
+    );
+}
+
+export default Menutab;
---
+++ src/components/Navbar.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "../App.css"
+
+function Navbar() {
+    return (
+
+        <div className="nav">
+            <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,600,0,0" />
+            <div className="logo">ELEVEN MEDIA</div>
+            <label for="burger" class="burger">
+  <input id="burger" type="checkbox" />
+  <span></span>
+  <span></span>
+  <span></span>
+</label>
+        </div>
+    );
+}
+
+export default Navbar;
