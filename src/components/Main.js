64ffeb339fa76ae73a632684ecf6c50cf654baa9
MrProgrammer00 <88051679+MrProgrammer00@users.noreply.github.com>
2023-01-11 18:09:19 +0530

    new

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,7 +4,10 @@ import "../App.css"
 function Main() {
     return(
         <div className="main">
-           
+           <div className="main-page1">
+            <h1>WELCOME <br></br>
+            To ELEVEN MEDIA</h1>
+           </div>
         </div>
     );
 }
